789726756b44867f25fe71d72651f5912ff461ec
nelsoncassemiro <nelsoncassemiro@gmail.com>
2023-02-11 00:22:24 -0300

    Iniciando Projeto

---
+++ Back/ProEventos.API/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+        public IEnumerable<Evento> _evento = new Evento[]
+        {
+              new Evento()
+            {
+                EventoId = 1,
+                Tema = "Angular e dotnet",
+                Local = "Fortaleza",
+                Lote = "1 lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString()
+                },
+              new Evento(){
+                EventoId = 2,
+                Tema = "Angular e suas novidades",
+                Local = "Sobral",
+                Lote = "2 lote",
+                QtdPessoas = 350,
+                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy")
+                }
+            };
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
+        }
+
+        [HttpPost]
+        public string Post() 
+        {
+            return "Exemplo de Post";
+        }
+    }
+
+}
