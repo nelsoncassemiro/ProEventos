0a02db530edb093a26a0db57c8f8ac1ea3614837
nelsoncassemiro <nelsoncassemiro@gmail.com>
2023-03-10 14:15:33 -0300

    Front Angular

---
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -0,0 +1,43 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-eventos',
+  templateUrl: './eventos.component.html',
+  styleUrls: ['./eventos.component.scss']
+})
+export class EventosComponent implements OnInit {
+
+  public eventos: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void{
+    this.http.get('https://localhost:44363/api/eventos').subscribe(
+      response => this.eventos = response,
+      error => console.log(error)
+
+    );
+    /*
+    this.eventos = [
+        {
+          Tema: 'Angular11',
+          Local: 'Belo Horizonte'
+        },
+        {
+          Tema: '.NET 5',
+          Local: 'Sobral'
+        },
+        {
+          Tema: 'Angular e suas novidades',
+          Local: 'Camocim'
+        }
+    ]
+    */
+  }
+
+}
